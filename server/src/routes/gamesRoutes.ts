5387cf8922e7afcc64e55f094b7f0cbe628a46c1
David Castillo <davidcastillo@Davids-MBP.fritz.box>
2019-11-07 12:59:42 +0100

    this is the first commit, the application is working correctly in the backend  as well in the fornt end

---
+++ server/src/routes/gamesRoutes.ts
@@ -0,0 +1,26 @@
+import express, { Router } from 'express';
+
+import gamesController from '../controllers/gamesController';
+
+class GameRoutes {
+
+    public router: Router = Router();
+
+    constructor() {
+        this.config();
+    }
+
+    config() {
+        this.router.get('/all', gamesController.listAll);
+        this.router.get('/', gamesController.list);
+        this.router.get('/:id', gamesController.getOne);
+        this.router.post('/', gamesController.create);
+        this.router.put('/:id', gamesController.update);
+        this.router.delete('/:id', gamesController.delete);
+    }
+
+}
+const gameRoutes = new GameRoutes()
+export default gameRoutes.router
+// export default new GameRoutes().router;
+
